4d46aae79fa022f635825da4e42b8aa0c90316dc
Laaiqah <133792671+pixelbub@users.noreply.github.com>
2025-10-07 09:49:29 +0200

    add images for opening and closing cutscene todo: full render for cutscene images

--- src/cutscenes/postLobbyCutscene.js
+++ src/cutscenes/postLobbyCutscene.js
@@ -1,27 +1,27 @@
 // src/cutscenes/postLobbyCutscene.js
 export const postLobbyCutscene = [
   {
-    image: "/assets/cutscenes/ending1.png",
+    image: "/assets/cutscenes/scene2.png",
     text: "When the smoke cleared, the bellboy ghost knelt among the debris, his glow soft and pale",
   },
   {
-    image: "/assets/cutscenes/ending2.png",
+    image: "/assets/cutscenes/scene2.png",
     text: "Bellboy: I—I’m sorry. I don’t know what came over me",
   },
   {
-    image: "/assets/cutscenes/ending3.png",
+    image: "/assets/cutscenes/scene2.png",
     text: "Bellboy: ...",
   },
   {
-    image: "/assets/cutscenes/ending3.png",
+    image: "/assets/cutscenes/scene3.png",
     text: "Bellboy: You’re cleaning? After all this time?",
   },
   {
-    image: "/assets/cutscenes/ending3.png",
+    image: "/assets/cutscenes/scene3.png",
     text: "Bellboy: Heh… guess the old place still has a chance.",
   },
   {
-    image: "/assets/cutscenes/ending3.png",
-    text: "Bellboy: Let me help. I still remember how to ring the front desk bell.",
+    image: "/assets/cutscenes/scene3.png",
+    text: "Bellboy: Let me help! I still remember how to run the front desk.",
   },
 ];
--- src/cutscenes/tutorialCutscene.js
+++ src/cutscenes/tutorialCutscene.js
@@ -1,6 +1,6 @@
 export const tutorialCutscene = [
-  { image: "/assets/cutscenes/scene1.jpeg", text: "One-star again..." },
-  { image: "/assets/cutscenes/scene1.jpeg", text: "‘Cold rooms, eerie noises’... " },
-  { image: "/assets/cutscenes/scene1.jpeg", text: "Maybe a little cleaning won’t hurt..." },
-  { image: "/assets/cutscenes/scene1.jpeg ", text: "A clean haunt is a happy haunt... right?" },
+  { image: "/assets/cutscenes/scene1.png", text: "One-star again..." },
+  { image: "/assets/cutscenes/scene1.png", text: "‘Cold rooms, eerie noises’... " },
+  { image: "/assets/cutscenes/scene1.png", text: "Maybe a little cleaning won’t hurt..." },
+  { image: "/assets/cutscenes/scene1.png ", text: "A clean haunt is a happy haunt... right?" },
 ];
