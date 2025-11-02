c430897b27ff7b7b48556ea3f81dc2ff67f54f4e
Laaiqah <133792671+pixelbub@users.noreply.github.com>
2025-11-02 19:55:45 +0200

    Update level3start.js

--- src/cutscenes/level3start.js
+++ src/cutscenes/level3start.js
@@ -1,7 +1,7 @@
 export const level3start = [
   { image: "/assets/cutscenes/kitchen1.png", text: "Business is finally picking up." },
-  { image: "/assets/cutscenes/kitchen2.png", text: "Guests actually stayed the night. Nobody screamed. Progress." },
-  { image: "/assets/cutscenes/kitchen3.png", text: "We've even started taking breakfast orders again." },
-  { image: "/assets/cutscenes/kitchen4.png", text: "Only problem?" },
-  { image: "/assets/cutscenes/kitchen5.png", text: "Something’s… cooking itself." },
+  { image: "/assets/cutscenes/kitchen1.png", text: "Guests actually stayed the night. Nobody screamed. Progress." },
+  { image: "/assets/cutscenes/kitchen1.png", text: "We've even started taking breakfast orders again." },
+  { image: "/assets/cutscenes/kitchen2.png", text: "Only problem?" },
+  { image: "/assets/cutscenes/kitchen2.png", text: "Something’s… cooking itself." },
 ];
