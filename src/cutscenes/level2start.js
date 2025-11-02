1cc4df62c23d19c307af21a314a7ec2df4f9bfdb
Laaiqah <133792671+pixelbub@users.noreply.github.com>
2025-11-02 13:27:28 +0200

    add cutscene sprites

--- src/cutscenes/level2start.js
+++ src/cutscenes/level2start.js
@@ -1,8 +1,8 @@
 export const level2start = [
-  { image: "/assets/cutscenes/scene1.png", text: "Ever since cleaning up a bit, we've been seeing more guests than usual" },
-  { image: "/assets/cutscenes/scene1.png", text: "Bellboy's also been a great help" },
-  { image: "/assets/cutscenes/scene1.png", text: "But new guests also means..." },
-  { image: "/assets/cutscenes/scene1.png ", text: "new complaints" },
-  { image: "/assets/cutscenes/scene1.png ", text: "Bellboy: Something's wrong with the plumbing" },
-  { image: "/assets/cutscenes/scene1.png ", text: "Guess I should see to that" },
+  { image: "/assets/cutscenes/bathroom1.png", text: "Ever since cleaning up a bit, we've been seeing more guests than usual" },
+  { image: "/assets/cutscenes/bathroom1.png", text: "Bellboy's also been a great help" },
+  { image: "/assets/cutscenes/bathroom2.png", text: "But new guests also means..." },
+  { image: "/assets/cutscenes/bathroom3.png", text: "new complaints" },
+  { image: "/assets/cutscenes/bathroom3.png", text: "Bellboy: Something's wrong with the plumbing" },
+  { image: "/assets/cutscenes/bathroom3.png", text: "Guess I should see to that" },
 ];
